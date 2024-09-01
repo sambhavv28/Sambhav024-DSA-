4003e480d42b796d2c56f5c207461070d9fed547
sambhavv28 <159689398+sambhavv28@users.noreply.github.com>
2024-09-01 21:35:19 +0530

    Add files via upload

---
+++ array_rec.cpp
@@ -0,0 +1,45 @@
+// Recursive implementation of Binary search
+#include <stdio.h>
+
+
+int a_search(int num[], int Left, int Right, int key) {
+    int Middle = 0;
+    while (Left <= Right) {
+        Middle = Left + (Right - Left) / 2;
+
+        if (num[Middle] == key){
+            return Middle;
+        }
+
+        if (num[Middle] > key){
+            return a_search(num, Left, Middle-1, key);
+        }
+
+        else{
+            return a_search(num, Middle+1, Right, key);
+        }
+    }
+
+    return -1;
+}
+int main(){
+
+    int size = 0, key = 0, found = 0;
+    printf("Enter the size of the array: ");
+    scanf("%d", & size);
+    int num[size];
+    printf("Enter the elements of the array in ascending order: ");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", & num[i]);
+    }
+    printf("Enter the element to be searched: ");
+    scanf("%d", & key);
+    found = a_search(num, 0, size - 1, key);
+    if (found == -1) {
+        printf("Element is not present in the array");
+    } 
+    else {
+        printf("Element found at index %d", found);
+    }
+    return 0;
+}
